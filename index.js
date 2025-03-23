5b868d95e3a2cc47d805c39a51872f93c65df883
Victor Furustubbe <mantykanto2@gmail.com>
2025-03-23 22:09:46 +0100

    added resp header w nav

--- index.js
+++ index.js
@@ -6,11 +6,18 @@ const PORT = 3001;
 
 app.set('view engine', 'ejs')
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    res.send('Hello from Homes Page!')
+    // res.send('Hello from Homes Page!')
+    res.render('pages/home.ejs', {
+        logotype: 'The Swiss Design School'
+    })
 })
 
 app.use('/typefaces', typefaceRouter);
 app.use('/designers', designerRouter);
+
+
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
